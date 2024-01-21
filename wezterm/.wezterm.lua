ba5c5e8741b6ea927937102a3f8a0c5c6c85d959
Eirik Humlen <95056805+humlen@users.noreply.github.com>
2024-01-21 11:34:29 +0100

    added panes

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,22 +7,45 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+-- font
 config.font = wezterm.font_with_fallback {
   'JetBrainsMono Nerd Font Mono'
 }
+config.font_size = 10
 
+-- keys
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
   -- Paste from clipboard
-  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-
-  -- Paste from the primary selection
-  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' }
+  { key = 'V',    mods = 'CTRL',    action = act.PasteFrom 'Clipboard' },
+  { key = 'V',    mods = 'CTRL',    action = act.PasteFrom 'PrimarySelection' },
+  { key = "s",    mods = 'LEADER',  action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "v",    mods = 'LEADER',  action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "h",    mods = "LEADER",  action = act.ActivatePaneDirection("Left") },
+  { key = "j",    mods = "LEADER",  action = act.ActivatePaneDirection("Down") },
+  { key = "k",    mods = "LEADER",  action = act.ActivatePaneDirection("Up") },
+  { key = "l",    mods = "LEADER",  action = act.ActivatePaneDirection("Right") },
+  { key = "q",    mods = "LEADER",  action = act.CloseCurrentPane { confirm = true } },
 }
 
 -- Set the Wezterm theme
 config.color_scheme = "Catppuccin Mocha"
 
 -- Set the Wezterm opacity
-config.window_background_opacity = 0.88
+config.window_background_opacity = 0.9
+
+-- Dim inactive panes
+config.inactive_pane_hsb = {
+  saturation = 0.24,
+  brightness = 0.50
+}
+
+
+-- Other
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
+config.scrollback_lines = 3000
+
+
 
 return config
